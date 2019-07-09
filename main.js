19c546bfd7bf6dadbcdaccfd445f125fe1e6263c
Juan Pablo Bovo <juampi@MacBook-Pro-de-Juan.local>
2019-07-09 17:22:02 +0200

    change color background on click

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ var inputs = document.getElementsByTagName('input');
 
 var input = inputs[0];
 
+var arrColors = ['red', 'orange', 'blue', 'red'];
+
+function rand(){
+    return Math.floor(Math.random() * arrColors.length); 
+}
+
 button.onclick = function(){
     console.log('click', input.value);
     alert('Hello ' + input.value + '!')
+    var bodyColor = document.getElementsByTagName('body')[0];
+    var number = rand();
+    bodyColor.style.backgroundColor = arrColors[number];
 };
